3bb5a9167cb3b060a5bf14c2e4ae7312e399c6ae
acarlotto <acarlotto@cox.net>
2017-08-02 13:49:29 -0400

    Tic Tac Toe Game Project
    Added code to make game alert a draw. :)

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,18 +4,22 @@
 let playerToken = 'x'
 // let countX = 0
 const playerTokenWin = 0
-let count = 0
+let count = parseInt($(this).data('click')) || 0
 let xWin = 0
 let oWin = 0
 
+$('.square').on('click', function () {
+  count = count + 1
+  $(this).data('click', count)
+})
+
+// let scores = [0, 0]
 $('.gameBoard').on('click', ".square:not('.square-x, .square-o')", function (event) {
   const $square = $(event.currentTarget)
   $square.addClass('square-' + playerToken)
 
-  if (checkIfPlayerWonX('square-' + playerToken)) {
-    setMessage('X' + 'has Won!')
-  } else if (checkIfPlayerWonO('square-' + playerToken)) {
-    setMessage('O' + 'has Won!')
+  if (checkIfPlayerWon('square-' + playerToken)) {
+    setMessage()
   } else if (playerToken === 'x') {
     playerToken = 'o'
     setMessage(playerToken + '\'s Turn')
@@ -26,52 +30,45 @@ $('.gameBoard').on('click', ".square:not('.square-x, .square-o')", function (eve
 })
 // Swap current player's token
 
+const checkIfPlayerWon = function (symbol) {
+  if ($('#r1').hasClass('square-x') && $('#r2').hasClass('square-x') && $('#r3').hasClass('square-x') ||
+     $('#r4').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r6').hasClass('square-x') ||
+     $('#r7').hasClass('square-x') && $('#r8').hasClass('square-x') && $('#r9').hasClass('square-x') ||
+     $('#r1').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r9').hasClass('square-x') ||
+     $('#r3').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r7').hasClass('square-x') ||
+     $('#r1').hasClass('square-x') && $('#r4').hasClass('square-x') && $('#r7').hasClass('square-x') ||
+     $('#r2').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r8').hasClass('square-x') ||
+     $('#r3').hasClass('square-x') && $('#r6').hasClass('square-x') && $('#r9').hasClass('square-x')) {
+    // alert('X Wins!') //  return xWin
+    alert('User X has won the game. Start a new game ')
+    $('.square').addClass('.square:before')
+    $('.square').removeClass('disable')
+    $('.square').removeClass('square-o')
+    $('.square').removeClass('square-x')
+  } else if ($('#r1').hasClass('square-x') && $('#r2').hasClass('square-x') && $('#r3').hasClass('square-x') ||
+     $('#r4').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r6').hasClass('square-o') ||
+     $('#r7').hasClass('square-o') && $('#r8').hasClass('square-o') && $('#r9').hasClass('square-o') ||
+     $('#r1').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r9').hasClass('square-x') ||
+     $('#r3').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r7').hasClass('square-o') ||
+     $('#r1').hasClass('square-o') && $('#r4').hasClass('square-o') && $('#r7').hasClass('square-o') ||
+     $('#r2').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r8').hasClass('square-o') ||
+     $('#r3').hasClass('square-o') && $('#r6').hasClass('square-o') && $('#r9').hasClass('square-o')) {
+  // alert('O Wins!') //  return xWin
+    alert('O wins has won the game. Start a new game')
+    $('.square').addClass('.square:before')
+    $('.square').removeClass('disable')
+    $('.square').removeClass('square-o')
+    $('.square').removeClass('square-x')
+  } else if (count === 9) {
+    alert('Tie Game')
+    $('.square').addClass('.square:before')
+    $('.square').removeClass('disable')
+    $('.square').removeClass('square-o')
+    $('.square').removeClass('square-x')
+  }
+}
 const setMessage = function (msg) {
   document.getElementById('message').innerText = msg
 }
-
-const checkIfPlayerWonX = function (symbol) {
-  if ($('#r1').hasClass('square-x') && $('#r2').hasClass('square-x') && $('#r3').hasClass('square-x')) {
-    alert('X Wins!') //  return xWin
-    count = 0
-    xWin++
-  } else {
-    return false
-  }
-}
-
-const checkIfPlayerWonO = function (symbol) {
-  if ($('#r1').hasClass('square-o') && $('#r2').hasClass('square-o') && $('#r3').hasClass('square-o')) {
-    alert('oWin') //  return xWin
-  } else {
-    return false
-  }
-}
-
-/* THIS IS THE POSSIBLE SOLUTION TO CHECK FOR TIE const checkForTie = function () {
-  if ($('#r1').hasClass('square-') && $('#r2').hasClass('square-') && $('#r3').hasClass('square-') &&
-  $('#r4').hasClass('square-') && $('#r5').hasClass('square-') && $('#r6').hasClass('square-') && $('#r7').hasClass('square-') &&
-  $('#r8').hasClass('square-') && $('#r9').hasClass('square-')) {
-    alert('Tie Game') //  return xWin
-  } else {
-    return false
-  }
-} */
-
-/* let winner = function () {
- if(!winner) {
-   //check to see if there is a tie
-   if(numMoves == 9)
-      status.innerHTML = 'Tie Game!';
- } else {
-   gameOver = true
-} */
-
-document.querySelector('.startNewGame').addEventListener('click', function () {
-  'use strict'
-  window.location.reload()
-}, true)
-// Start the game
-// window.onload = _startGame()
 
 module.exports = true
