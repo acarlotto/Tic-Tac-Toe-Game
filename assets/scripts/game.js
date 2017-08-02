5e5a6ae12f9b561c185575ce64e3d7e80b060c1c
acarlotto <acarlotto@cox.net>
2017-08-02 16:16:02 -0400

    Tic Tac Toe Game Project
    Added code to calculate wins of x and o.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,8 +5,18 @@ let playerToken = 'x'
 // let countX = 0
 const playerTokenWin = 0
 let count = parseInt($(this).data('click')) || 0
-let xWin = 0
-let oWin = 0
+let xWin = 1
+let oWin = 1
+
+const calculateWinsX = function () {
+  xWin = xWin + 1
+  return xWin
+}
+
+const calculateWinsO = function () {
+  oWin = oWin + 1
+  return oWin
+}
 
 $('.square').on('click', function () {
   count = (count + 1)
@@ -45,10 +55,13 @@ const checkIfPlayerWon = function (symbol) {
     $('.square').removeClass('disable')
     $('.square').removeClass('square-o')
     $('.square').removeClass('square-x')
+    $('#xWin').text(xWin)
+    calculateWinsX()
+    count = 0
   } else if ($('#r1').hasClass('square-o') && $('#r2').hasClass('square-o') && $('#r3').hasClass('square-o') ||
      $('#r4').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r6').hasClass('square-o') ||
      $('#r7').hasClass('square-o') && $('#r8').hasClass('square-o') && $('#r9').hasClass('square-o') ||
-     $('#r1').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r9').hasClass('square-x') ||
+     $('#r1').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r9').hasClass('square-o') ||
      $('#r3').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r7').hasClass('square-o') ||
      $('#r1').hasClass('square-o') && $('#r4').hasClass('square-o') && $('#r7').hasClass('square-o') ||
      $('#r2').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r8').hasClass('square-o') ||
@@ -59,6 +72,9 @@ const checkIfPlayerWon = function (symbol) {
     $('.square').removeClass('disable')
     $('.square').removeClass('square-o')
     $('.square').removeClass('square-x')
+    $('#oWin').text(oWin)
+    calculateWinsO()
+    count = 0
   } else if (count === 9) {
     alert('Tie Game')
     $('.square').addClass('.square:before')
@@ -71,5 +87,13 @@ const checkIfPlayerWon = function (symbol) {
 const setMessage = function (msg) {
   document.getElementById('message').innerText = msg
 }
+
+$('#reset').click(function () {
+  $('.square').addClass('.square:before')
+  $('.square').removeClass('disable')
+  $('.square').removeClass('square-o')
+  $('.square').removeClass('square-x')
+  count = 0
+})
 
 module.exports = true
