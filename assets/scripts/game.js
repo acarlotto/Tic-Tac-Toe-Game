72e4b6b0ab8c48b650d7562a23729d52d87deb7c
acarlotto <acarlotto@cox.net>
2017-08-02 22:20:26 -0400

    Tic Tac Toe Game Project
    edited code to take away alert boxes.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,5 +1,5 @@
 'use strict'
-// test
+
 // Player X will start the game
 let playerToken = 'x'
 // let countX = 0
@@ -29,7 +29,11 @@ $('.gameBoard').on('click', ".square:not('.square-x, .square-o')", function (eve
   $square.addClass('square-' + playerToken)
 
   if (checkIfPlayerWon('square-' + playerToken)) {
-    setMessage()
+    setMessage('Yay!' + playerToken + 'has won the game. Start a new game')
+  } else if (checkIfPlayerWon === 'square-x') {
+    setMessage('User X has won! Start a new game.')
+  } else if (checkIfPlayerWon === 'square-o') {
+    setMessage('User O has won! Start a new game.')
   } else if (playerToken === 'x') {
     playerToken = 'o'
     setMessage(playerToken + '\'s Turn')
@@ -49,15 +53,10 @@ const checkIfPlayerWon = function (symbol) {
      $('#r1').hasClass('square-x') && $('#r4').hasClass('square-x') && $('#r7').hasClass('square-x') ||
      $('#r2').hasClass('square-x') && $('#r5').hasClass('square-x') && $('#r8').hasClass('square-x') ||
      $('#r3').hasClass('square-x') && $('#r6').hasClass('square-x') && $('#r9').hasClass('square-x')) {
-    // alert('X Wins!') //  return xWin
-    alert('User X has won the game. Start a new game ')
-    $('.square').addClass('.square:before')
-    $('.square').removeClass('disable')
-    $('.square').removeClass('square-o')
-    $('.square').removeClass('square-x')
     $('#xWin').text(xWin)
     calculateWinsX()
     count = 0
+    return ('square-x')
   } else if ($('#r1').hasClass('square-o') && $('#r2').hasClass('square-o') && $('#r3').hasClass('square-o') ||
      $('#r4').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r6').hasClass('square-o') ||
      $('#r7').hasClass('square-o') && $('#r8').hasClass('square-o') && $('#r9').hasClass('square-o') ||
@@ -67,14 +66,10 @@ const checkIfPlayerWon = function (symbol) {
      $('#r2').hasClass('square-o') && $('#r5').hasClass('square-o') && $('#r8').hasClass('square-o') ||
      $('#r3').hasClass('square-o') && $('#r6').hasClass('square-o') && $('#r9').hasClass('square-o')) {
   // alert('O Wins!') //  return xWin
-    alert('O wins has won the game. Start a new game')
-    $('.square').addClass('.square:before')
-    $('.square').removeClass('disable')
-    $('.square').removeClass('square-o')
-    $('.square').removeClass('square-x')
     $('#oWin').text(oWin)
     calculateWinsO()
     count = 0
+    return ('square-o')
   } else if (count === 9) {
     alert('Tie Game')
     $('.square').addClass('.square:before')
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,6 +9,15 @@ $(() => {
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const gameEvents = require('./game')
 
+$(() => {
+  $('#sign-up').on('submit', gameEvents.onsignUp)
+})
 // use require without a reference to ensure a file is bundled
-require('./game.js')
+/* const gameEvents = require('./game.js')
+$() => {
+  // click handlers for buttons and forms
+  $('$register').on('click', gameEvents.loadRegsitration)
+  $('$registration').on(submit, gameEvents.registeredUser)
+}) */
